091d057d86ecab0b799b2933e57d59be69db482a
JASIYANOUFAL <jasiyanoufal8317@gmail.com>
2024-12-19 21:22:41 +0530

    sample programe class added

---
+++ src/test/java/org/Stepdefinition/SampleProgram.java
@@ -0,0 +1,35 @@
+package org.Stepdefinition;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleProgram {
+
+	@Test
+	private void tc1() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://en-gb.facebook.com/");
+		driver.manage().window().maximize();
+		}
+	@Test
+	private void tc2() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+
+	}
+	@Test
+	private void tc3() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		
+
+	}
+}
